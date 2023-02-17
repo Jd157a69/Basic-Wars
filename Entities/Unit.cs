d5f44b955c6bc6eb1e81aa83620130e80b7bb837
Jd157a69 <jd157a69@gmail.com>
2023-02-17 17:49:44 +0000

    Added method for setting unit attributes Unit attributes are now set when a unit is created. A method for resetting the movementPoints is required for each turn.

--- Entities/Unit.cs
+++ Entities/Unit.cs
@@ -13,26 +13,30 @@ namespace Basic_Wars_V2.Entities
 {
     public class Unit : IGameEntity, ICollideable
     {
-        public const int UNIT_WIDTH = 56;
-        public const int UNIT_HEIGHT = 56;
-        public const int X_SPRITE_SHEET_START_POS = 0;
-        public const int Y_SPRITE_SHEET_START_POS = 0;
-        public const int SPRITE_SHEET_TEAM_SHIFT = 168;
-        public const int SPRITE_SHEET_UNIT_SHIFT = 56;
+        private const int UNIT_WIDTH = 56;
+        private const int UNIT_HEIGHT = 56;
+
+        private const int X_SPRITE_SHEET_START_POS = 0;
+        private const int Y_SPRITE_SHEET_START_POS = 0;
+
+        private const int ANIMATION_SHIFT = 56;
+
+        private const int SPRITE_SHEET_TEAM_SHIFT = 168;
+        private const int SPRITE_SHEET_UNIT_SHIFT = 56;
 
         public Sprite unitSprite;
 
-        public Texture2D Texture { get; set; }
+        private Texture2D Texture { get; set; }
+
         public Vector2 Position { get; set; }
         public UnitState State { get; set; }
       
         public int Team { get; set; }
-        public int UnitType { get; set; }
-
+        public UnitType Type { get; set; }
 
         public int Health { get; set; }
-        public int RemainingMoves { get; set; }
-        public int MovementRange { get; set; }
+        public int MovementPoints { get; set; }
+        public int CostToProduce { get; set; }
 
         
         public int ID { get; set; }
@@ -41,22 +45,18 @@ namespace Basic_Wars_V2.Entities
 
         public Unit(Texture2D texture, Vector2 position, int unitType, int team)
         {
-            this.Texture = texture;
-            this.Position = position;
-            this.Team = team;
-            this.UnitType = unitType;
+            Texture = texture;
+            Position = position;
+            Team = team;
 
-            State = UnitState.Idle;
-            CreateUnitSprite();
-
-            //Temporary
-            Health = 10;
+            CreateUnitSprite(unitType);
+            SetUnitAttributes(unitType);
         }
 
-        public void CreateUnitSprite()
+        public void CreateUnitSprite(int unitType)
         {
             int teamShift = (Team - 1) * SPRITE_SHEET_TEAM_SHIFT;
-            int unitShift = (UnitType - 1) * SPRITE_SHEET_UNIT_SHIFT;
+            int unitShift = (unitType - 1) * SPRITE_SHEET_UNIT_SHIFT;
             unitSprite = new Sprite(Texture, X_SPRITE_SHEET_START_POS + teamShift, Y_SPRITE_SHEET_START_POS + unitShift, UNIT_WIDTH, UNIT_HEIGHT);
         }
 
@@ -73,61 +73,42 @@ namespace Basic_Wars_V2.Entities
             unitSprite.Draw(_spriteBatch, Position);
         }
 
-        public void CheckHealth()
+        public void SetUnitAttributes(int unitType)
         {
-            if (Health <= 0)
+            Health = 100;
+            State = UnitState.Idle;
+
+            switch (unitType - 1)
             {
-                State = UnitState.Dead;
-            }
-        }
-
-        public void CheckState()
-        {
-
-            //Use of Unit.State should be elsewhere instead of inside the unit class
-
-            switch (State)
-            {
-                case UnitState.Idle:
-
-                    CheckHealth();
-
+                case 0:
+                    Type = UnitType.Infantry;
+                    CostToProduce = 1000;
+                    MovementPoints = 3;
                     break;
 
-                case UnitState.Selected:
-
-                    //Display options to user of what unit can do               
-
+                case 1:
+                    Type = UnitType.Mech;
+                    CostToProduce = 3000;
+                    MovementPoints = 2;
                     break;
 
-                case UnitState.Moving:
-
-                    //State = UnitState.Idle;
-
-                    //Display tiles unit can move to
-                    //In InputController find the position of the mouse and relate it to the tile grid -> (MousePos - Offset of map position) % 56 
-                    //Change Position of selected unit to match Position of selected tile
-                    //unitSprite.TintColour = UnitUsed;
-
+                case 2:
+                    Type = UnitType.Tank;
+                    CostToProduce = 7000;
+                    MovementPoints = 6;
                     break;
 
-                case UnitState.Attacking:
-
-                    //Show potential targets that the unit can attack
-
-                    unitSprite.TintColour = UnitUsed;
-
-                    break;
-
-                default:
+                case 3:
+                    Type = UnitType.APC;
+                    CostToProduce = 5000;
+                    MovementPoints = 6;
                     break;
             }
         }
-
 
         public void Update(GameTime gameTime)
         {
-            CheckState();
+
         }
 
     }
