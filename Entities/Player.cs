015462c56d46ea62b2b4c3f38c47aa08a3d7ffa0
Jd157a69 <jd157a69@gmail.com>
2023-02-25 18:13:15 +0000

    Added Player class

---
+++ Entities/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Wars_V2.Entities
+{
+    public class Player
+    {
+        public int Team { get; set; }
+        public string Name { get; set; }
+        public int Funds { get; set; }
+
+        public Player(int team, int initialFunds)
+        {
+            Team = team;
+            Funds = initialFunds;
+        }
+    }
+}
