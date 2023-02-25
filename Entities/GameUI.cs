1f63a803b14b5aab70b357a2e2e87aeb8d300e78
Jd157a69 <jd157a69@gmail.com>
2023-02-25 18:13:02 +0000

    Moving and Attacking Adjusted the GetReachableTiles method to block the movement of vehicles to mountains. New method, GetAttackableTiles, uses GetNeighbours from MapManager to search adjacent tiles for enemy units. New method, AttackUnit, calculates damage dealt to a defending unit by an attacking unit with the use of a dictionary which stores damage values dependent on UnitType in a tuple. New method, DamageCalculation, for use in AttackUnit.
    AttackUnit does not currently work, figuring out why

--- Entities/GameUI.cs
+++ Entities/GameUI.cs
@@ -15,14 +15,33 @@ namespace Basic_Wars_V2.Entities
     public class GameUI : IGameEntity
     {
         private Tile Selected_UI;
-        private bool UnitSelected;
-        private bool TileSelected;
 
-        private Unit SelectedUnit;
+        private bool UnitSelected = false;
+        private bool TileSelected = false;
+
+        private Unit SelectedUnit; 
         private Tile SelectedTile;
 
         private List<Tile> reachableTiles = new List<Tile>();
-        private List<Tile> reachableOverlay = new List<Tile>();
+        private List<Tile> moveableOverlay = new List<Tile>();
+        private List<Tile> attackableOverlay = new List<Tile>();
+        private List<Tile> tilesToBeRemoved = new List<Tile>();
+
+        private Dictionary<(UnitType, UnitType), int> baseDamageDictionary = new Dictionary<(UnitType, UnitType), int>()
+        { 
+                {(UnitType.Infantry, UnitType.Infantry), 55 },
+                {(UnitType.Infantry, UnitType.Mech), 45 },
+                {(UnitType.Infantry, UnitType.Tank), 5 },
+                {(UnitType.Infantry, UnitType.APC), 14 },
+                {(UnitType.Mech, UnitType.Infantry), 65 },
+                {(UnitType.Mech, UnitType.Mech), 55 },
+                {(UnitType.Mech, UnitType.Tank), 55 },
+                {(UnitType.Mech, UnitType.APC), 75 },
+                {(UnitType.Tank, UnitType.Infantry), 75 },
+                {(UnitType.Tank, UnitType.Mech), 70 },
+                {(UnitType.Tank, UnitType.Tank), 55 },
+                {(UnitType.Tank, UnitType.APC), 100 },
+        };
 
         public Texture2D Texture;
         public SpriteFont Font;
@@ -45,24 +64,22 @@ namespace Basic_Wars_V2.Entities
             _buttonManager = new ButtonManager();
             _inputController = new InputController(_unitManager, _buttonManager, _gameMap);
             _pathFinder = new Dijkstra(_gameMap);
-
-
+            
             //      TESTING
-            /*Button TitleButton = new Button(texture, font, new Vector2(1080/2, 50), "Basic Wars", "Menu");
-            Button NewGame = new Button(texture, font, new Vector2(1080/2, 270), "New Game", "Menu");
-            Button LoadGame = new Button(texture, font, new Vector2(1080/2, 420), "Load Game", "Menu");
-            Button Quit = new Button(texture, font, new Vector2(1080 / 2, 570), "Quit", "AltMenu");
+            //Button TitleButton = new Button(Texture, font, new Vector2((1920 - 672) / 2, 90), "Basic Wars", "Menu");
+            //Button NewGame = new Button(Texture, font, new Vector2((1920 - 672) / 2, 360), "New Game", "Menu");
+            //Button LoadGame = new Button(Texture, font, new Vector2((1920 - 672) / 2, 540), "Load Game", "Menu");
+            //Button Quit = new Button(Texture, font, new Vector2((1920 - 672) / 2, 720), "Quit", "Menu");
 
 
-            _buttonManager.AddButton(TitleButton);
-            _buttonManager.AddButton(NewGame);
-            _buttonManager.AddButton(LoadGame);
-            _buttonManager.AddButton(Quit);
-            */
+            //_buttonManager.AddButton(TitleButton);
+            //_buttonManager.AddButton(NewGame);
+            //_buttonManager.AddButton(LoadGame);
+            //_buttonManager.AddButton(Quit);
+
 
             Selected_UI = new Tile(new Vector2(0, 0), Texture);
             Selected_UI.CreateTile(0, 1);
-
         }
 
         private void GetSelected()
@@ -75,11 +92,116 @@ namespace Basic_Wars_V2.Entities
                 Selected_UI.Position= SelectedUnit.Position;
                 UnitSelected = true;
             }
-            else if (SelectedTile != null)
+            if (SelectedTile != null)
             {
-                Selected_UI.Position= SelectedTile.Position;
+                Selected_UI.Position = SelectedTile.Position;
                 SelectedTile.State = TileState.None;
                 TileSelected = true;
+            }
+        }
+
+        private void GetReachableTiles()
+        {
+            if (UnitSelected)
+            {
+                moveableOverlay.Clear();
+
+                Tile startingTile = GetCurrentUnitTile(SelectedUnit);
+                
+                reachableTiles = _pathFinder.FindReachableTiles(startingTile, SelectedUnit);
+
+                //Blocking movement onto tiles already containing another unit
+                foreach (Unit unit in _unitManager.units)
+                {
+                    foreach (Tile tile in reachableTiles)
+                    {
+                        if (unit.Position == tile.Position && SelectedUnit != unit)
+                        {
+                            tilesToBeRemoved.Add(tile);
+                        }
+                    }
+                }
+                foreach (Tile tile in tilesToBeRemoved)
+                {
+                    reachableTiles.Remove(tile);
+                }
+                tilesToBeRemoved.Clear();
+
+
+                foreach (Tile tile in reachableTiles)
+                {
+                    if (tile.Position != SelectedUnit.Position)
+                    {
+                        if (
+                            !(tile.Type == TileType.Mountain 
+                            && SelectedUnit.Type == UnitType.Tank) 
+                            && !(tile.Type == TileType.Mountain 
+                            && SelectedUnit.Type == UnitType.APC)
+                           )
+                        {
+                            Tile overlayTile = new Tile(tile.Position, Texture);
+                            overlayTile.CreateTile(2, 1);
+                            moveableOverlay.Add(overlayTile);
+                        }
+                        else
+                        {
+                            tilesToBeRemoved.Add(tile);
+                        }
+                    }
+                }
+
+                //Blocking display of mountain tiles for vehicles
+                foreach (Tile tile in tilesToBeRemoved)
+                {
+                    moveableOverlay.Remove(tile);
+                }
+                tilesToBeRemoved.Clear();
+            }
+        }
+
+        private void GetAttackableTiles()
+        {
+            if (UnitSelected)
+            {
+                attackableOverlay.Clear();
+
+                Tile currentUnitTile = GetCurrentUnitTile(SelectedUnit); 
+                List<Tile> adjacentTiles = _gameMap.GetNeighbours(currentUnitTile); 
+                
+                foreach (Tile tile in adjacentTiles)
+                {
+                    foreach (Unit unit in _unitManager.units)
+                    {
+                        if (
+                            unit.Position == tile.Position 
+                            && SelectedUnit.Team != unit.Team 
+                            && SelectedUnit.Type != UnitType.APC
+                           )
+                        {
+                            Tile attackingTile = new Tile(tile.Position, Texture);
+                            attackingTile.CreateTile(1, 1);
+                            attackableOverlay.Add(attackingTile);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DisplayAttributes()
+        {
+            if (UnitSelected)
+            {
+                Unit previousUnit = SelectedUnit;
+                Console.WriteLine($"Type: {previousUnit.Type}");
+                Console.WriteLine($"Health: {previousUnit.Health}");
+                Console.WriteLine($"Ammo: {previousUnit.Ammo}");
+                Console.WriteLine($"Movement Range: {previousUnit.MovementPoints}");
+            }
+            if (TileSelected)
+            {
+                Tile previousTile = SelectedTile;
+                Console.WriteLine($"Type: {previousTile.Type}");
+                Console.WriteLine($"Defence Bonus: {previousTile.DefenceBonus}");
             }
         }
 
@@ -90,10 +212,12 @@ namespace Basic_Wars_V2.Entities
                 _inputController.UpdateMouseState();
                 foreach (Tile tile in _gameMap.map)
                 {
-                    if (_inputController.MouseCollider.Intersects(tile.Collider)
+                    if (
+                        _inputController.MouseCollider.Intersects(tile.Collider)
                         && _inputController.currentMouseState.LeftButton == ButtonState.Pressed
                         && _inputController.previousMouseState.LeftButton == ButtonState.Released
-                        && reachableTiles.Contains(tile))
+                        && reachableTiles.Contains(tile)
+                       )
                     {
                         SelectedUnit.Position = tile.Position;
                         SelectedUnit.State = UnitState.Idle;
@@ -103,56 +227,27 @@ namespace Basic_Wars_V2.Entities
             }
         }
 
-        private void ReachableTiles()
+        private void AttackUnit()
         {
-            if (UnitSelected)
+            while (UnitSelected)
             {
-                reachableOverlay.Clear();
-
-                Tile startingTile = null;
-
-                foreach (Tile tile in _gameMap.map)
+                _inputController.UpdateMouseState();
+                foreach (Unit unit in _unitManager.units)
                 {
-                    if (SelectedUnit.Position == tile.Position)
+                    if (
+                        _inputController.MouseCollider.Intersects(unit.Collider)
+                        && _inputController.currentMouseState.LeftButton == ButtonState.Pressed
+                        && _inputController.previousMouseState.LeftButton == ButtonState.Released
+                        && SelectedUnit.Team != unit.Team
+                        && attackableOverlay.Contains(GetCurrentUnitTile(unit))
+                       )
                     {
-                        startingTile = tile;
-                    }
-                }
-                
-                reachableTiles = _pathFinder.FindReachableTiles(startingTile, SelectedUnit);
-
-                foreach (Tile tile in reachableTiles)
-                {
-                    if (tile.Position != SelectedUnit.Position)
-                    {
-                        if (!(tile.Type == TileType.Mountain && SelectedUnit.Type == UnitType.Tank) && !(tile.Type == TileType.Mountain && SelectedUnit.Type == UnitType.APC))
-                        {
-                            Tile overlayTile = new Tile(tile.Position, Texture);
-                            overlayTile.CreateTile(2, 1);
-                            reachableOverlay.Add(overlayTile);
-                        }
+                        SelectedUnit.Ammo--;
+                        unit.Health -= DamageCalculation(SelectedUnit, unit);
                     }
                 }
             }
         }
-
-        //private void DisplayAttributes()
-        //{
-        //    if (UnitSelected)
-        //    {
-        //        Unit previousUnit = SelectedUnit;
-        //        Console.WriteLine($"Type: {previousUnit.Type}");
-        //        Console.WriteLine($"Health: {previousUnit.Health}");
-        //        Console.WriteLine($"Ammo: {previousUnit.Ammo}");
-        //        Console.WriteLine($"Movement Range: {previousUnit.MovementPoints}");
-        //    }
-        //    if (TileSelected)
-        //    {
-        //        Tile previousTile = SelectedTile;
-        //        Console.WriteLine($"Type: {previousTile.Type}");
-        //        Console.WriteLine($"Defence Bonus: {previousTile.DefenceBonus}");
-        //    }
-        //}
 
         private void CheckForUnitGeneration()
         {
@@ -161,10 +256,11 @@ namespace Basic_Wars_V2.Entities
             if (TileSelected)
             {
                 TileSelected = false;
-                if (SelectedTile.Type == TileType.Factory)  //Add team check as well later
+                if (SelectedTile.Type == TileType.Factory)
                 {
                     // Using console for now
                     // UI implmentation after frame is done
+                    //Add team check as well later
 
                     Console.WriteLine("Enter unit to be produced:\n1. Infantry\n2. Mech\n3. Tank\n4. APC");
                     //int unitType = Convert.ToInt32(Console.ReadLine());
@@ -176,28 +272,60 @@ namespace Basic_Wars_V2.Entities
             }
         }
 
-        private void UpdateUI()
+        private int DamageCalculation(Unit attackingUnit, Unit defendingUnit)
         {
-            GetSelected();
-            //DisplayAttributes();
-            ReachableTiles();
+            int damage = 0;
+
+            int baseDamage = baseDamageDictionary[(attackingUnit.Type, defendingUnit.Type)];
+            int defendingUnitDefenceBonus = GetCurrentUnitTile(defendingUnit).DefenceBonus;
+
+            damage = baseDamage - (int)(baseDamage * (defendingUnitDefenceBonus/100));
+
+            return damage;
         }
 
+        private Tile GetCurrentUnitTile(Unit unit)
+        {
+            foreach (Tile tile in _gameMap.map)
+            {
+                if (unit.Position == tile.Position)
+                {
+                    return tile;
+                }
+            }
+            return null;
+        } 
+
+        private void UpdateUI()
+        { 
+
+            GetSelected();
+
+            //DisplayAttributes();
+
+            GetReachableTiles();
+            GetAttackableTiles();
+        }
+
+        private void UpdateGameActions()
+        {
+            MoveUnit();
+            AttackUnit();
+            //Tile selection breaks after this method is run because in the draw method TileSelected will always be false due to the CheckForUnitGeneration running
+            //CheckForUnitGeneration();
+        }
 
         public void Update(GameTime gameTime)
         {
             _inputController.ProcessControls(gameTime);
 
-            MoveUnit();
-
-            //Checking for unit generation gets stuck: when a unit is generated and you move the unit: the tile is registered as being clicked again
-            //CheckForUnitGeneration();
+            UpdateGameActions();
 
             UpdateUI();
         }
 
         public void Draw(SpriteBatch _spriteBatch, GameTime gameTime)
-        {
+        {       
             if (UnitSelected || TileSelected)
             {
                 Selected_UI.Draw(_spriteBatch, gameTime);
@@ -205,11 +333,20 @@ namespace Basic_Wars_V2.Entities
 
             if (UnitSelected)
             {
-                foreach (Tile tile in reachableOverlay)
+                foreach (Tile tile in moveableOverlay)
+                {
+                    tile.Draw(_spriteBatch, gameTime);
+                }
+                foreach (Tile tile in attackableOverlay)
                 {
                     tile.Draw(_spriteBatch, gameTime);
                 }
             }
+
+            //foreach (Button button in _buttonManager.buttons)
+            //{
+            //    button.Draw(_spriteBatch, gameTime);
+            //}
         }
 
 
