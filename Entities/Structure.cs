20f637a66a4fc2ed6672bdd656fa4dbd8af99ae9
Jd157a69 <jd157a69@gmail.com>
2023-03-07 18:09:05 +0000

    Added Structure object

---
+++ Entities/Structure.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Wars_V2.Entities
+{
+    public class Structure : Tile
+    {
+        public int Team { get; set; }
+
+        public Structure(Vector2 position, Texture2D texture, int team) :base(position, texture)
+        {
+            Team = team;
+        }
+    }
+}
