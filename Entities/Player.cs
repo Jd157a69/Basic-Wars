c22f6ca7d2f9fa5632e629e669c186426839c611
Jd157a69 <jd157a69@gmail.com>
2023-03-13 17:58:12 +0000

    Added setting colours to identify player

--- Entities/Player.cs
+++ Entities/Player.cs
@@ -8,16 +8,40 @@ namespace Basic_Wars_V2.Entities
 {
     public class Player
     {
-        public int Team { get; set; }
-        public string Name { get; set; }
+        public int Team { get; private set; }
         public int Funds { get; set; }
         public bool HasHQ { get; set; }
+        public string Colour { get; private set; }
 
         public Player(int team, int initialFunds)
         {
             Team = team;
             Funds = initialFunds;
             HasHQ = true;
+
+            GetTeamColour();
+        }
+
+        private void GetTeamColour()
+        {
+            switch (Team)
+            {
+                case 0:
+                    Colour = "Red";
+                    break;
+
+                case 1:
+                    Colour = "Blue";
+                    break;
+
+                case 2:
+                    Colour = "Green";
+                    break;
+
+                case 3:
+                    Colour = "Yello";
+                    break;
+            }
         }
     }
 }
