304010f4f98b77c21313e9d47a8dfc3cef9659fc
Jd157a69 <jd157a69@gmail.com>
2023-03-14 11:19:31 +0000

    Removed TileState in favour of bool Selected Removed switch case statement for creating tile on type -> moved to Tile class

--- System/TileData.cs
+++ System/TileData.cs
@@ -14,7 +14,6 @@ namespace Basic_Wars_V2.System
     public class TileData
     {
         public TileType Type { get; set; }
-        public TileState State { get; set; }
 
         public Vector2 Position { get; set; }
         public Vector2 MapGridPos { get; set; }
@@ -26,7 +25,6 @@ namespace Basic_Wars_V2.System
         public TileData(Tile tile)
         {
             Type = tile.Type;
-            State = tile.State;
             Position = tile.Position;
             MapGridPos = tile.MapGridPos;
             Team = tile.Team;
@@ -36,30 +34,7 @@ namespace Basic_Wars_V2.System
         {
             Tile tile = new Tile(Position, Texture, Team);
             tile.Type = Type;
-            tile.State = State;
             tile.MapGridPos = MapGridPos;
-
-            switch (tile.Type)
-            {
-                case TileType.Plains:
-                    tile.CreateTileSprite();
-                    break;
-                case TileType.Forest:
-                    tile.CreateTileSprite(1, 0);
-                    break;
-                case TileType.Mountain:
-                    tile.CreateTileSprite(2, 0);
-                    break;
-                case TileType.City:
-                    tile.CreateTileSprite(-5 + Team, 0);
-                    break;
-                case TileType.Factory:
-                    tile.CreateTileSprite(-5 + Team, 1);
-                    break;
-                case TileType.HQ:
-                    tile.CreateTileSprite(-5 + Team, 2);
-                    break;
-            }
 
             return tile;
         }
